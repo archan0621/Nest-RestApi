688810fae7f6766de00cd349d9199c3809fbe76e
archan <joshua_0621@naver.com>
2021-05-17 10:45:06 +0900

    init

---
+++ dist/movies/movies.controller.d.ts
@@ -0,0 +1,8 @@
+export declare class MoviesController {
+    getAll(): string;
+    search(): string;
+    getOne(id: string): string;
+    create(movieData: any): any;
+    remove(id: string): string;
+    path(id: string, updateData: any): any;
+}
